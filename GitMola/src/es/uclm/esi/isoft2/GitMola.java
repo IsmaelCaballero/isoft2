f3953ceff82ab546e0e7056764b4a3b6163e100a
Ismael Caballero <Ismael.Caballero@uclm.es>
2016-09-01 23:03:14 +0200

    Primera Versión del Programa Git Mola

---
+++ GitMola/src/es/uclm/esi/isoft2/GitMola.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package es.uclm.esi.isoft2;
+
+/**
+ * @author Ismae
+ *
+ */
+public class GitMola {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
